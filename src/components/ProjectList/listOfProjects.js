6a22b679375e3d2628b8617d40dd92c4d9c7d977
Kanishk Pachauri <krishna03012003@icloud.com>
2022-08-13 17:01:08 +0000

    Updated Project List

--- src/components/ProjectList/listOfProjects.js
+++ src/components/ProjectList/listOfProjects.js
@@ -1,11 +1,55 @@
 const projectList = [
+
   {
-    name: 'Find me Mate',
-    imageSrc: 'https://static.dezeen.com/uploads/2017/08/tinder-redesign-graphics_dezeen_sq-1.jpg',
-    projectLink: 'https://github.com/resetercss/reseter.css',
-    description: 'A tinder like dating app which mainly focus on College Students , Because There is no app which focus on dating of Inter Colleges',
-    tags: ['css', 'sass', 'scss', 'less', 'stylus', 'django']
-  },
+    name: "AI Chatbot to answer FAQs",
+    imageSrc: "https://cdn.analyticsvidhya.com/wp-content/uploads/2018/01/chatbot-inbenta.jpg",
+    projectLink: "https://github.com/FOSS-Community/KPB-Projects/blob/main/AI-Chatbot.md",
+    description: "An AI-Based chatbot to answer some basic FAQs",
+    tags: ["AI", "Chatbot", "Beginner"]
+  } ,
+  {
+    name: "Graphical Password Authentication",
+    imageSrc: "https://3.imimg.com/data3/PG/IT/MY-11200775/graphical-password-authentication-using-cued-click-points-250x250.jpg",
+    projectLink: "https://github.com/FOSS-Community/KPB-Projects/blob/main/Graphical-Password.md",
+    description: "Instead of creating a password a user has to select graphical objects in a particular order to keep it as their password",
+    tags: ["Blockchain", "Cybersecurity", "Intermediate"]
+  } ,
+  {
+    name: "Diet Recall Application",
+    imageSrc: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoca9Y2vBdVyXpr8ucK8qOfXjWBceKjwk9PQ&usqp=CAU",
+    projectLink: "https://github.com/FOSS-Community/KPB-Projects/blob/main/Diet-Recall.md",
+    description: "Instead of creating a password a user has to select graphical objects in a particular order to keep it as their password",
+    tags: ["HealthCare", "Beginner", "App Development"]
+  } ,
+  
+    {
+      name: "Ticketless Entry System",
+      imageSrc: "https://images.squarespace-cdn.com/content/v1/584ee3cc2994cac9e545aadd/1596821614007-SASZ45FE6IPAJPWAG54M/flat-design-illustration-concepts-online-cinema-ticket-order-hand-holding-mobile-smart-phone-with-online-buy-app-modern-flat-creative-info-graphics-design_143407-777.jpg?format=500w",
+      projectLink: "https://github.com/FOSS-Community/KPB-Projects/blob/main/Ticketless-Entry.md",
+      description: " Ticketless Entry System to Monuments and Museums enabled with a QR feature that allows the verification of visitors at the entry of the monument or the museum by scanning the QR code on their E-tickets.",
+      tags: ["Tourism", "Intermediate"]
+    } , 
+    {
+      name: "AR Based Classroom",
+      imageSrc: "https://www.ebsco.com/sites/g/files/nabnos191/files/acquiadam-assets/top-ten-augmented-reality-tools-blog-image-780.jpg",
+      projectLink: "https://github.com/FOSS-Community/KPB-Projects/blob/main/AR-Classroom.md",
+      description: "Augmented reality models can be attached to a book",
+      tags: ["AR", "Beginner", "Augmented Reality"]
+    } ,
+    {
+      name: "AI-enhanced OT security tool",
+      imageSrc: "https://mma.prnewswire.com/media/1781520/Enterprises__Need_for_OT_Security_Expertise.jpg?p=facebook",
+      projectLink: "https://github.com/FOSS-Community/KPB-Projects/blob/main/AI-OT.md",
+      description: "AI based tool to help discover devices and hidden patterns while processing large amounts of real-time pipeline operational data",
+      tags: ["AI", "Beginner"]
+    } ,
+    {
+      name: "Cyber Hygiene for students",
+      imageSrc: "https://images.digitalguardian.com/sites/default/files/cyber_hygiene.png",
+      projectLink: "https://github.com/FOSS-Community/KPB-Projects/blob/main/Cyber-Hygiene.md",
+      description: "Devise a way to impart knowledge in children about cyber hygiene by fabrication of a cyber network that can be sustained through crowdsourcing.",
+      tags: ["Beginner"]
+    },
 ];
 export default projectList;
 
