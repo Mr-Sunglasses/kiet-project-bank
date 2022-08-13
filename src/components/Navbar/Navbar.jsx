d59a0c3daf81da9cdaecfbf5f9814c7a39548c91
Kanishk Pachauri <krishna03012003@icloud.com>
2022-08-13 17:21:57 +0000

    Added Navbar Components:

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,19 +8,19 @@ import github from './github.svg';
 const Navbar = () => {
   return (
       <div className="topnav">
-          <a href="https://join.slack.com/t/firstcontributors/shared_invite/zt-iywfifau-_aMtdwTjBoMzQqzW8~YUUA"  target="_blank" rel="noopener noreferrer">
+          <a href="https://join.slack.com/t/fossc-u/shared_invite/zt-1e9vt5hgk-k5GGNMb7r8mvxXfgbexdvQ"  target="_blank" rel="noopener noreferrer">
               <img src={slack} className="logo" alt="slack logo" />
               <span>Slack</span>
           </a>
-          <a href="https://www.youtube.com/channel/UCMXNFxCvyH5LhUwEcmY8qGQ" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.youtube.com/channel/UC_h3B9nyJu7s8TMHiccVELw" target="_blank" rel="noopener noreferrer">
               <img  src={youtube} className="logo" alt="youtube logo" />
               <span>Youtube</span>    
           </a>
-          <a href="https://twitter.com/1stContribution" target="_blank" rel="noopener noreferrer">
+          <a href="https://twitter.com/fosscuk" target="_blank" rel="noopener noreferrer">
               <img   src={twitter} className="logo" alt="twitter logo" />
               <span>Twitter</span>
           </a>
-          <a href="https://github.com/firstcontributions/first-contributions" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/FOSS-Community/KPB-Projects" target="_blank" rel="noopener noreferrer">
               <img  src={github} className="logo" alt="github logo" />
               Github
           </a>
